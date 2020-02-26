e6c00816088b5b5b00cd7939b864221d90a581fd
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-02-26 23:15:48 +0900

    [UPDATE] Force override on build

--- public/electron.js
+++ public/electron.js
@@ -6,11 +6,12 @@ const isDev = require("electron-is-dev");
 let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 900, height: 680 });
-  mainWindow.loadURL(
+  /*mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
-  );
+  );*/
+  mainWindow.loadURL("http://localhost:3000");
   mainWindow.setFullScreen(true);
   mainWindow.setMenu(null);
   mainWindow.on("closed", () => (mainWindow = null));
