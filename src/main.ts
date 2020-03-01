0b0f40472cc93136fc97d140918c0d5edc2aa1cf
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-03-01 23:58:42 +0900

    [UPDATE] fix ts build error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // Modules to control application life and create native browser window
 import {app, BrowserWindow} from 'electron';
-import path from 'path';
+import * as path from 'path';
 
 function createWindow () {
   // Create the browser window.
