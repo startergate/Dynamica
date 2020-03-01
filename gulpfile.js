6333aea8a927718463bbf6f8a9f796a31d8b05cb
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-03-02 00:42:08 +0900

    [MISC] use gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const sourcemaps = require("gulp-sourcemaps")
+
+const tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("default", _ => {
+  gulp.src([
+    './src/**/*.html',
+    './src/**/*.js',
+  ]).pipe(gulp.dest('build'));
+  return tsProject.src()
+    .pipe(tsProject())
+    .js
+      .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest("build"));
+});
