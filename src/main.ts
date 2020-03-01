deb0aa9f16e8e1ff2598c7223f53e055b58a6dd0
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-03-02 01:47:10 +0900

    [MISC] ES6 style

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import {app, BrowserWindow, screen} from 'electron';
 import * as path from 'path';
 
-function createWindow () {
+const createWindow = () => {
   // Create the browser window.
   const { width, height } = screen.getPrimaryDisplay().workAreaSize
   const mainWindow = new BrowserWindow({
@@ -22,7 +22,7 @@ function createWindow () {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
-}
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -30,16 +30,16 @@ function createWindow () {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
 // In this file you can include the rest of your app's specific main process
