b20de73e909b7aaff604cac96b07e610892c78ad
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-03-10 22:15:35 +0900

    [CREATE] some files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
-const sourcemaps = require("gulp-sourcemaps")
+const sourcemaps = require("gulp-sourcemaps");
 
 const tsProject = ts.createProject("tsconfig.json");
 
@@ -8,6 +8,7 @@ gulp.task("default", _ => {
   gulp.src([
     './src/**/*.html',
     './src/**/*.js',
+    './src/**/*.css',
   ]).pipe(gulp.dest('build'));
   return tsProject.src()
     .pipe(tsProject())
