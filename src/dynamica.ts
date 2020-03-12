9fcf3a3a86df947795f93f000d19c4389c11c6e0
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-03-13 03:20:02 +0900

    [UPDATE] dynamica.js
    convert to typescript
    remove useless functions

---
+++ src/dynamica.ts
@@ -0,0 +1,15 @@
+import { ipcRenderer } from 'electron';
+import IpcRendererEvent = Electron.IpcRendererEvent;
+
+const current = document.getElementById('current');
+const next = document.getElementById('next');
+
+current.addEventListener('transitionend', () => {
+  current.setAttribute('src', next.getAttribute('src'));
+  current.classList.remove('hide');
+});
+
+ipcRenderer.on('change-image', (event: IpcRendererEvent, path: string) => {
+  next.setAttribute('src', path);
+  current.classList.add('hide');
+});
