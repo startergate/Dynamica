bb349c740a2255d3bc9b0906ee1393f80766a1f4
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-05-02 02:11:19 +0900

    [UPDATE] os 모듈 import로 변경

--- src/utils/initialize.ts
+++ src/utils/initialize.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as os from 'os';
 
 import { Image } from '../models/image';
 
-const settingDir = path.join(require('os').homedir(), 'Documents', 'Dynamica');
+const settingDir = path.join(os.homedir(), 'Documents', 'Dynamica');
 export const settingFile = path.join(settingDir, 'setting.json');
 
 export const initialize = () : Map<string, Image> => {
