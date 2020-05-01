a0fa18834ef71b16346f694373dd4e3400370a46
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-05-02 01:42:23 +0900

    [UPDATE] Loads setting file

--- src/utils/initialize.ts
+++ src/utils/initialize.ts
@@ -2,13 +2,17 @@ import * as path from "path";
 import * as fs from "fs";
 import {Image} from "../models/image";
 
-const settingDir = path.join(require('os').homedir(), 'Dynamica', 'setting.json');
+
+const settingDir = path.join(require('os').homedir(), 'Documents', 'Dynamica');
+export const settingFile = path.join(settingDir, 'setting.json');
 
 export const initialize = () : Map<string, Image> => {
   try {
-    return JSON.parse(fs.readFileSync(settingDir).toString());
+    const save = fs.readFileSync(settingFile).toString();
+    return JSON.parse(save);
   } catch (e) {
-    fs.writeFileSync(settingDir, '{}');
+    fs.mkdirSync(settingDir)
+    fs.writeFileSync(settingFile, '{}', { flag: 'w+' });
     return initialize();
   }
 };
