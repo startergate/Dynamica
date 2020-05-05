01ec7cee839bbb2af4745b734e795d144dedda8e
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-05-05 22:35:49 +0900

    [CREATE] creating setting data base code

--- src/renderer/option.ts
+++ src/renderer/option.ts
@@ -2,6 +2,12 @@ import { ipcRenderer } from 'electron';
 
 const submitBtn = document.getElementById('submit');
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  const targetTable = document.getElementById('originValue');
+  const originValues = targetTable.getElementsByClassName('schedule');
+  for (const value of originValues) {
+    console.log(value);
+  }
   ipcRenderer.send('update-setting', 'something');
 })
