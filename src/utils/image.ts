30df8a229be1cbbcb41d6405c6c2417fd42f6af1
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-05-09 16:53:45 +0900

    [UPDATE] 이미지 가져올 때 뜨던 에러 수정

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -5,9 +5,8 @@ let setting: Setting = initialize();
 
 export const getImage = () => {
   const now: Date = new Date;
-  for (let time in setting) {
-    if (setting.hasOwnProperty(time))
-      if (parseInt(time) <= now.getHours() * 100 + now.getMinutes()) return setting.get(time).image;
+  for (let [time, image] of Object.entries(setting)) {
+    if (parseInt(time) <= now.getHours() * 100 + now.getMinutes()) return image.image;
   }
   return null;
 };
