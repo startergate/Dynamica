d117679d229f7d5ce476998d12610a6fd844e4a0
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-05-09 14:41:55 +0900

    [UPDATE] use background on div instead of img

--- src/renderer/dynamica.ts
+++ src/renderer/dynamica.ts
@@ -2,18 +2,11 @@ import { ipcRenderer } from 'electron';
 import IpcRendererEvent = Electron.IpcRendererEvent;
 
 const current = document.getElementById('current');
-const next = document.getElementById('next');
 const skipButton = document.getElementById('skipThisImage');
 const settingButton = document.getElementById('settingTab');
 
-current.addEventListener('transitionend', () => {
-  current.setAttribute('src', next.getAttribute('src'));
-  current.classList.remove('hide');
-});
-
 ipcRenderer.on('change-image', (event: IpcRendererEvent, path: any) => {
-  next.setAttribute('src', path);
-  current.classList.add('hide');
+  current.style.backgroundImage = path;
 });
 
 skipButton.addEventListener('click', () => {
